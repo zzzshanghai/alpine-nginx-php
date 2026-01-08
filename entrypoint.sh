10135127fbfe970a333b77de88d8dddb4ba78dae
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2026-01-08 21:59:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/usr/bin/supervisord -c /root/supervisord.conf
-chown -R www-data:www-data
+chown -R www-data:www-data /var/www/html
 chmod 755 /var/www/html
+/usr/bin/supervisord -c /root/supervisord.conf
 
 exec "$@"
